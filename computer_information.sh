503cb4331dae1a4ab2991192a24cd68a4cf0bfd8
Movsisyan Hovhannes <movsisyanh@live.com>
2020-12-17 17:57:14 +0400

    Added ram information with  function.

--- computer_information.sh
+++ computer_information.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+function get_ram_info()
+{
+	ram_info=`free -h | grep "Mem:" | awk '{ print "Total: \t" $2 "\n" "Free: \t" $4 "\n" "Used: \t" $3 } '`
+	echo "$ram_info"
+}
+
 if [ $# -eq 0 ]
 then
 	echo -e "Please specify an argument."
 	exit 1
 fi
 
+if [ $1 = "-r" ]
+then
+	ram_info="$(get_ram_info)"
+        echo "$ram_info"	
+fi
 
 
 exit 0
+
